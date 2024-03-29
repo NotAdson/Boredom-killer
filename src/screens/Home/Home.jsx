eb9455a12de7bdc49b0ea60a7c3c5e2667fb3fd6
Adson Gabriel <adsongabrielfsilva@gmail.com>
2024-03-29 18:22:26 +0000

    Feat(Home.jsx): adding media type buttons

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,9 +1,26 @@
 import { useState } from 'react'
-import controllerLogo from './assets/controller.svg'
-import bookLogo from './assets/book.svg'
-import film from './assets/film.svg'
+import controllerLogo from '../../assets/controller.svg'
+import bookLogo from '../../assets/book.svg'
+import film from '../../assets/film.svg'
+import MediaTypeButton from '../../components/MediaTypeButtons/MediaTypeButton'
 import './Home.css'
 
-function App() {
-  return ()
+function random(){
+  console.log("Oi")
+}
+
+export default function Home() {
+  return (
+    <>
+        <div align="center" >
+            <MediaTypeButton imagePath={controllerLogo} handleFunction={random} />
+            <MediaTypeButton imagePath={bookLogo} handleFunction={random} />
+            <MediaTypeButton imagePath={film} handleFunction={random} />
+        </div>
+        <div>
+            
+        </div>
+    </>
+
+  )
 }
