ae756124e9ec14d24664f40d2d913b3d8e502537
Adson Gabriel <adsongabrielfsilva@gmail.com>
2024-04-01 20:22:57 +0100

    Feat(Home.jsx): adding card system

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -3,22 +3,41 @@ import controllerLogo from '../../assets/controller.svg'
 import bookLogo from '../../assets/book.svg'
 import film from '../../assets/film.svg'
 import MediaTypeButton from '../../components/MediaTypeButtons/MediaTypeButton'
+import Card from '../../components/Card/Card'
 import './Home.css'
 
-function random(){
-  console.log("Oi")
-}
-
 export default function Home() {
+  const [cards, setCards] = useState([])
+
+  async function getGames(){
+    const response = await fetch("http://localhost:8009/games");
+    const data = await response.json();
+    setCards(data.list)
+  }
+  
+  async function getMovies(){
+    const response = await fetch("http://localhost:8009/movies");
+    const data = await response.json();
+    setCards(data.list)
+  }
+  
+  async function getBooks(){
+    const response = await fetch("http://localhost:8009/books");
+    const data = await response.json();
+    setCards(data.list)
+  }
+  
   return (
     <>
         <div align="center" >
-            <MediaTypeButton imagePath={controllerLogo} handleFunction={random} />
-            <MediaTypeButton imagePath={bookLogo} handleFunction={random} />
-            <MediaTypeButton imagePath={film} handleFunction={random} />
+            <MediaTypeButton imagePath={controllerLogo} handleFunction={getGames} />
+            <MediaTypeButton imagePath={bookLogo} handleFunction={getBooks} />
+            <MediaTypeButton imagePath={film} handleFunction={getMovies} />
         </div>
         <div>
-            
+          {cards.map(card =>(
+            <Card title={card.title} description={card.description}/>
+          ))}
         </div>
     </>
 
